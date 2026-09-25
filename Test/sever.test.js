7b64623344f03241832d78b781095e0bd8995623
Baarrun <mbaarrun2008@gmail.com>
2026-09-25 16:25:50 +0500

    Updated Code

--- Test/sever.test.js
+++ Test/sever.test.js
@@ -1,38 +1,19 @@
 const request = require('supertest');
-const server = require('./server');
+const server = require('../server');
 
-describe('GET /get API', () => {
+describe('GET /get API Test', () => {
 
-  test('Should return status code 200', async () => {
-    const response = await request(server).get('/get');
+  test('Should return Hello message', async () => {
+
+    const response = await request(server)
+      .get('/get');
 
     expect(response.statusCode).toBe(200);
-  });
-
-  test('Should return correct JSON response', async () => {
-    const response = await request(server).get('/get');
 
     expect(response.body).toEqual({
       Hello: 'say hello'
     });
-  });
 
-  test('Should contain Hello property', async () => {
-    const response = await request(server).get('/get');
-
-    expect(response.body).toHaveProperty('Hello');
-  });
-
-  test('Should return correct content type', async () => {
-    const response = await request(server).get('/get');
-
-    expect(response.headers['content-type']).toMatch(/json/);
-  });
-
-  test('Should not allow POST request on /get', async () => {
-    const response = await request(server).post('/get');
-
-    expect(response.statusCode).toBe(404);
   });
 
 });
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express')
 var server = express()
-server.get('/get',async,(req,res)=>{
+server.get('/get',async (req,res)=>{
   res.json({"Hello":"say hello"})
 });
- server.listen(5000, () => {
-    console.log(`Server is running on port `);
- });
+
+module.exports = server
